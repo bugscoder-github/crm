1a4d2a824fbb97c891f3123fe28779e8de76a50b
bugscoder <leecheetat@gmail.com>
2024-07-09 13:57:31 +0800

    add role seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,7 +22,7 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         $this->call([
-            // RolesSeeder::class,
+            RolesSeeder::class,
             UserSeeder::class,
         ]);
     }

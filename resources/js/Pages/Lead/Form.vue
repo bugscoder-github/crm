ee4a5f7ce47e3d91dd85c59de8140e00e82c2d6c
bugscoder <leecheetat@gmail.com>
2024-07-10 02:14:53 +0800

    remove lead todo list

--- resources/js/Pages/Lead/Form.vue
+++ resources/js/Pages/Lead/Form.vue
@@ -258,11 +258,11 @@
 				<div class="row">
 					TODO:
 						<ol>
-							<li>Customer extra information, full address, terms</li>
-				   			<li>salesperson able to choose existing customer</li>
 							<li>salesperson may pick from unassigned pool</li>
 							<li>salesperson may accept/reject lead</li>
 							<li>reopen Done Status</li>
+							<!-- <li>salesperson able to choose existing customer</li> -->
+							<!-- <li>Customer extra information, full address, terms</li> -->
 							<!-- <li>unable to edit/delete once it's marked as done</li> -->
 							<!-- <li>move leadcomment and lead function in 1 service class</li> -->
 							<!-- <li>lead comment should have web user that cannot be deleted by anyone</li> -->

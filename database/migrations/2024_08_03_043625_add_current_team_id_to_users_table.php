5229eddaee4b888440cc5a2f251d04cb7ee0fb62
bugscoder <leecheetat@gmail.com>
2024-08-04 13:57:39 +0800

    nullable current team id

--- database/migrations/2024_08_03_043625_add_current_team_id_to_users_table.php
+++ database/migrations/2024_08_03_043625_add_current_team_id_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->unsignedBigInteger('current_team_id')->after('remember_token');
+            $table->unsignedBigInteger('current_team_id')->after('remember_token')->nullable();
         });
     }
 
